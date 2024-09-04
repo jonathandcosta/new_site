05aa0ea6a5113f621ce1b7b7bb0a78b933efa14c
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-09-04 17:03:50 -0300

    chore: first estruture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import './App.css';
+import Main from './Content/Main';
+import Content from './Content/Content';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <>
+      <div className="main">
+        <Content />
+        <Main />
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/Content/Main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = () => {
+  return <div>Conteúdo Principal</div>;
+};
+
+export default Main;
