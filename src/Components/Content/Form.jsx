e0d75f537b1bd6c2ea0315d3d8ebd315011e7673
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-10-11 15:39:51 -0300

    chore: finished form contact and email contact

--- src/Components/Content/Form.jsx
+++ src/Components/Content/Form.jsx
@@ -1,42 +1,80 @@
 import React from 'react';
 import style from '../Content/Form.module.css';
+import Send from '../../Assests/icons/send.svg?react';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [mensagem, setMensagem] = React.useState('');
 
+  function sendEmail(e) {
+    e.preventDefault();
+
+    if (nome === '' || email === '' || mensagem === '')
+      alert('Fill in all fields');
+
+    const templateParams = {
+      from_name: nome,
+      email: email,
+      message: mensagem,
+    };
+
+    emailjs
+      .send(
+        'service_8gen6so',
+        'template_ftejs44',
+        templateParams,
+        'irq-jEKexZDvm62Fh',
+      )
+      .then(
+        (response) => {
+          console.log('Email enviado', response.status, response.text);
+          setNome('');
+          setEmail('');
+          setMensagem('');
+          alert('Sent successfully!');
+        },
+        (err) => {
+          console.log('Unexpected Error, please submit again.', err);
+        },
+      );
+  }
+
   return (
     <section className="container">
-      <p className="subtitle">Contact me Now</p>
-      <div className={style.content}>
-        <form>
-          <label htmlFor="nome">Nome</label>
+      <p className="subtitle">Want customized solutions?</p>
+      <p className="description">Let me know your contact and project!</p>
+      <form onSubmit={sendEmail}>
+        <div className={style.namesForm}>
           <input
+            placeholder="Full Name"
             id="nome"
             type="text"
             name="nome"
             value={nome}
             onChange={(event) => setNome(event.target.value)}
           />
-          <label htmlFor="email">Email</label>
           <input
+            placeholder="Email adress"
             id="email"
             type="text"
             name="email"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
-          <label htmlFor="mensagem">Mensagem</label>
-          <textarea
-            id="mensagem"
-            value={mensagem}
-            rows="5"
-            onChange={({ target }) => setMensagem(target.value)}
-          ></textarea>
-          <button>Enviar</button>
-        </form>
-      </div>
+        </div>
+        <textarea
+          placeholder="Your Message"
+          id="mensagem"
+          value={mensagem}
+          rows="10"
+          onChange={({ target }) => setMensagem(target.value)}
+        ></textarea>
+        <button>
+          <Send /> Send Message
+        </button>
+      </form>
     </section>
   );
 };
