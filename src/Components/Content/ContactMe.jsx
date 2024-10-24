f524a2985fdb7db112a5f3608b65de1253f273c0
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-10-24 11:13:49 -0300

    change

--- src/Components/Content/ContactMe.jsx
+++ src/Components/Content/ContactMe.jsx
@@ -9,29 +9,30 @@ const ContactMe = () => {
       </p>
       <div className={style.card}>
         <p className={style.titulo}>
-          Do you want a leader for your e-commerce?
+          ✔ Do you want a leader for your e-commerce?
         </p>
         <p className={style.description1}>
           I transform your e-commerce with leading performance and optimization
           strategies. If you are looking to see real results and improve your
-          website's results and experience, I can help. Let's work together! I
-          offer complete consultancy for your e-commerce, covering all fronts of
-          action, from the development and implementation of functionalities to
-          the optimization of processes and sales strategies.
+          website's results and experience, I can help. Let's work together!
+          <br /> <br /> I offer complete consultancy for your e-commerce,
+          covering all fronts of action, from the development and implementation
+          of functionalities to the optimization of processes and sales
+          strategies.
         </p>
       </div>
       <div className={style.card}>
         <p className={style.titulo}>
-          Do you need a Frontend developer for your project?
+          ✔ Do you need a Frontend developer for your project?
         </p>
         <p className={style.description1}>
           I am specialized in developing front-end, creating and integrating new
           layouts and features, as well as modern and efficient interfaces. I
           have experience in configuring marketing emails and automations to
-          enhance campaigns, always working collaboratively with design teams. I
-          also perform website maintenance, integrating payment systems,
-          databases and ERP, ensuring that all operations work seamlessly and
-          securely.
+          enhance campaigns, always working collaboratively with design teams.
+          <br /> <br /> I also perform website maintenance, integrating payment
+          systems, databases and ERP, ensuring that all operations work
+          seamlessly and securely.
         </p>
       </div>
       <div>
