113df7ba10e4e090cb9cf0df4b178662caf6f813
Jonathan Costa <jonathanduartecosta@gmail.com>
2024-10-24 14:43:16 -0300

    chore: news changes

--- src/Components/Content/Form.jsx
+++ src/Components/Content/Form.jsx
@@ -41,40 +41,88 @@ const Form = () => {
   }
 
   return (
-    <section className="container">
-      <p className="subtitle">Want customized solutions?</p>
-      <p className="description">Let me know your contact and project!</p>
-      <form onSubmit={sendEmail}>
-        <div className={style.namesForm}>
-          <input
-            placeholder="Full Name"
-            id="nome"
-            type="text"
-            name="nome"
-            value={nome}
-            onChange={(event) => setNome(event.target.value)}
-          />
-          <input
-            placeholder="Email Adress"
-            id="email"
-            type="text"
-            name="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
+    <>
+      <section className="container">
+        <p className="subtitle">
+          Your search for a qualified professional ends here!
+        </p>
+        <div className={style.card}>
+          <p className={style.titulo}>
+            ✔ Do you want a leader for your e-commerce?
+          </p>
+          <p className={style.description1}>
+            I transform your e-commerce with leading performance and
+            optimization strategies. If you are looking to see real results and
+            improve your website's results and experience, I can help. Let's
+            work together!
+            <br /> <br /> I offer complete consultancy for your e-commerce,
+            covering all fronts of action, from the development and
+            implementation of functionalities to the optimization of processes
+            and sales strategies.
+          </p>
         </div>
-        <textarea
-          placeholder="Your Message"
-          id="mensagem"
-          value={mensagem}
-          rows="10"
-          onChange={({ target }) => setMensagem(target.value)}
-        ></textarea>
-        <button>
-          <Send /> Send Message
-        </button>
-      </form>
-    </section>
+        <div className={style.card}>
+          <p className={style.titulo}>
+            ✔ Do you need a Frontend developer for your project?
+          </p>
+          <p className={style.description1}>
+            I am specialized in developing front-end, creating and integrating
+            new layouts and features, as well as modern and efficient
+            interfaces. I have experience in configuring marketing emails and
+            automations to enhance campaigns, always working collaboratively
+            with design teams.
+            <br /> <br /> I also perform website maintenance, integrating
+            payment systems, databases and ERP, ensuring that all operations
+            work seamlessly and securely.
+          </p>
+        </div>
+        <div>
+          <p className={style.finaly}>
+            If you need a professional who combines strategic vision, advanced
+            technical skills and a focus on results, or are looking for
+            specialized consulting for e-commerce, get in touch. Let's develop
+            solutions together that will take your business to the next level!
+          </p>
+        </div>
+      </section>
+      <section className="container">
+        <p className="subtitle">Want customized solutions?</p>
+        <p className="description">Let me know your contact and project!</p>
+        <form onSubmit={sendEmail}>
+          <div className={style.namesForm}>
+            <input
+              placeholder="Full Name"
+              id="nome"
+              required
+              type="text"
+              name="nome"
+              value={nome}
+              onChange={(event) => setNome(event.target.value)}
+            />
+            <input
+              placeholder="Email Adress"
+              id="email"
+              required
+              type="text"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <textarea
+            placeholder="Your Message"
+            id="mensagem"
+            required
+            value={mensagem}
+            rows="10"
+            onChange={({ target }) => setMensagem(target.value)}
+          ></textarea>
+          <button>
+            <Send /> Send Message
+          </button>
+        </form>
+      </section>
+    </>
   );
 };
 
--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Form from '../Content/Form';
-import ContactPit from '../Contact/Contact_pit';
 
 const Contact = () => {
   return (
     <section className="anime">
       <div className="border_page">
         <Header />
-        <ContactPit />
         <Form />
       </div>
     </section>
